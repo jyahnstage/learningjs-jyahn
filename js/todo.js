a04b897012ae1d793f69c2655f9f0092f7ff9133
jyahnstage <jyahnstage@gmail.com>
2023-04-03 03:37:01 +0900

    ongoing

---
+++ js/todo.js
@@ -0,0 +1,47 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = document.querySelector("#todo-form input");
+
+let toDos = [];
+
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+  
+}
+function deleteToDo(event) {
+    const willBeDeleted = event.target.parentElement;
+    willBeDeleted.remove();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "🙅‍♂️";
+    button.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
+
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem("todos");
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
